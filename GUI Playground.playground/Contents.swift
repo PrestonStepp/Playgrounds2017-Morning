41c8c7fb3a2cc99683488306294cd6a5f031cb74
PrestonStepp <31608773+PrestonStepp@users.noreply.github.com>
2017-10-04 09:00:12 -0600

    Added a second button.

--- GUI Playground.playground/Contents.swift
+++ GUI Playground.playground/Contents.swift
@@ -4,7 +4,12 @@ import UIKit
 
 var str = "Working with GUI"
 var firstButton = UIButton(frame: CGRect(x: 5, y:50, width: 180, height:100))
-firstButton.backgroundColor = .orange
+firstButton.backgroundColor = .red
 firstButton.setTitle("Button", for:.normal)
 var temp = firstButton.titleLabel
-firstButton.setTitle("Look a button", for: .normal)
+firstButton.setTitle("Look, a button.", for: .normal)
+var secondButton = UIButton(frame: CGRect(x: 5, y: 50, width: 180, height:100))
+secondButton.backgroundColor = .purple
+secondButton.setTitle("Just press it.", for:.normal)
+var currentTitle = secondButton.titleLabel
+secondButton.setTitle("Click me!!! NOW!", for: .normal)
