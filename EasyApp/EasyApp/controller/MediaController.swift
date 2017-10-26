f9e98496c8830341cdd587f37ca1c18bcf1a5e12
PrestonStepp <31608773+PrestonStepp@users.noreply.github.com>
2017-10-26 07:58:49 -0600

    Delegate app stuff

---
+++ EasyApp/EasyApp/controller/MediaController.swift
@@ -0,0 +1,19 @@
+//
+//  MediaController.swift
+//  EasyApp
+//
+//  Created by Stepp, Preston on 10/24/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+
+public class MediaController : UIViewController
+{
+    private var soundPlaye : AVAudioPlayer?
+    private var inageCounter : Int = 0
+    private lazy var color : ColorTools = ColorTools()
+    
+}
